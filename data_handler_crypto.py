2dd0334511afa96efbb9091d46c47cb674b4da5d
Robson Tavares Júnior <robsont@me.com>
2018-03-08 23:53:13 -0300

    Classe data_handler_crypto foi adicionada ao projeto para lidarmos com os dados provenientes da bitfinex, extremamente funcional

--- data_handler_crypto.py
+++ data_handler_crypto.py
@@ -5,7 +5,9 @@ import time
 
 class Data:
 
-    def __init__(self, ticker, start, end):
+    def __init__(self, ticker, start, end, interval='1h'):
+
+        self.interval = interval
 
         #Converting datetime to timestamp
 
@@ -16,25 +18,26 @@ class Data:
         self.startTS = start
         self.startTS = self.startTS.timetuple()
         #Correcting for my timezone
-        self.startTS = int(time.mktime(self.startTS)) * 1000
+        self.startTS = int(time.mktime(self.startTS) - self.epochTS) * 1000
 
         self.endTS = end
         self.endTS = self.endTS.timetuple()
-        self.endTS = int(time.mktime(self.endTS)) * 1000
+        self.endTS = int(time.mktime(self.endTS) - self.epochTS) * 1000
 
         #Structuring request's url
         self.ticker = ticker
-        self.url = "https://api.bitfinex.com/v2/candles/trade:5m:t" + self.ticker + "/hist"
-        response = requests.request("GET", self.url, params={'start': int(self.startTS), 'end': int(self.endTS)})
+        self.url = "https://api.bitfinex.com/v2/candles/trade:" + self.interval + ":t" + self.ticker + "/hist"
+        response = requests.request("GET", self.url, params={'limit': 1000, 'start': int(self.startTS), 'end': int(self.endTS)})
 
 
         #handling data
         self.rawData = response.json()
         self.cryptoData = pd.DataFrame(self.rawData)
-        # self.cryptoData.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
-        # self.cryptoData = self.cryptoData['Time'].apply(lambda x: dt.datetime.fromtimestamp(x)/10)
+        self.cryptoData.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+        self.cryptoData = self.cryptoData['Time'].apply(lambda x: dt.datetime.fromtimestamp(x/1000))
 
-
-
-data = Data('BTCUSD', dt.datetime(2018, 1, 1), dt.datetime(2018, 3, 6))
-
+        self.C = self.cryptoData['Close']
+        self.H = self.cryptoData['High']
+        self.L = self.cryptoData['Low']
+        self.V = self.cryptoData['Volume']
+        self.O = self.cryptoData['Open']
